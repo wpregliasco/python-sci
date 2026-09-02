b3b5418405a181b2734436b3ad0f7421ec34bf37
Willy Pregliasco <willy.pregliasco@gmail.com>
2026-09-01 21:38:17 -0300

    Update content (2026-09-01 21:38)

--- serve.sh
+++ serve.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 set -uo pipefail
 
+# Opción -x: matar el servidor sin cambiar de directorio.
+while getopts "x" opt; do
+  case $opt in
+    x)
+      pkill -f "quartz"
+      rm -f /tmp/quartz-serve.pid
+      exit 0
+      ;;
+    *)
+      echo "Uso: $0 [-x] [args...]" >&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND - 1))
+
 # Hacer disponibles node, npm y npx aunque el entorno no tenga el PATH de fnm.
 export PATH="/home/willy/.local/share/fnm/node-versions/v22.22.2/installation/bin:$PATH"
 
